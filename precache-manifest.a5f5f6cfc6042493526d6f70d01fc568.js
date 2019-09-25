6855a07c659538c995742665f45759a067304a41
Tamara Milovanovic <tamara.milovanovic@maibornwolff.de>
2019-09-26 00:12:14 +0200

    Updates

---
+++ precache-manifest.a5f5f6cfc6042493526d6f70d01fc568.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f60af277fffab0f441ede2875fc7e48",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "3bb96764d111f2dda6d4",
+    "url": "/me/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1a6cdf6140b6b42160a0",
+    "url": "/me/static/js/2.0a1290ad.chunk.js"
+  },
+  {
+    "revision": "3bb96764d111f2dda6d4",
+    "url": "/me/static/js/main.968a9bc5.chunk.js"
+  },
+  {
+    "revision": "6e714ea4306a70f2706a",
+    "url": "/me/static/js/runtime-main.2de94b03.js"
+  }
+]);
